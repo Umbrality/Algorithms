742b11bd076ec7f0e5e389fd3f30800408425aca
HSA-Student <student.e.amofa@horiozncolumbus.org>
2021-02-18 09:14:16 -0500

    yes

---
+++ src/org/horizoncolumbus/hs/Sort.java
@@ -0,0 +1,17 @@
+package org.horizoncolumbus.hs;
+
+public class Sort {
+
+    public static void main(String[] args) {
+
+        int[] unsortedArray = new int[]{5, 32, 6, 2, 0, -3, 45, 11, 85};
+
+        for (int index = 0; index < unsortedArray.length; index++) {
+            System.out.print(unsortedArray[index] + "\t");
+        }
+
+        int[] sortedArray = new int[unsortedArray.length];
+
+
+    }
+}
